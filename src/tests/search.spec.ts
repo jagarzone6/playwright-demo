f0c1c831b5004716a4962390985e5d2d2b2c385d
Jorge Garzon <jorge_garzon@epam.com>
2026-05-20 12:00:30 -0500

    update

--- src/tests/search.spec.ts
+++ src/tests/search.spec.ts
@@ -5,14 +5,11 @@ import { Page } from '@playwright/test';
 test('Search for Products', async ({ techShopPage: techShopSite }) => {
   //await searchProduct(techShopSite, "Key")
   await searchProductAlternative(techShopSite, "Key")
+  const one = await isProductPresent2(techShopSite, "Mouse");
+  const two = await isProductPresent2(techShopSite, "Keyboard");
+  const three = await isProductPresent2(techShopSite, "Moonitor");
+  console.log();
 });
-
-type Product = {
-  category: string,
-  title: string,
-  price: number,
-  inStock: boolean
-}
 
 async function searchProduct(page: Page, query: string) {
   const request = page.waitForRequest(/search/, { timeout: 10000, });
@@ -29,4 +26,14 @@ async function searchProductAlternative(page: Page, query: string) {
   await expect(TechShopPage.products(page)).not.toHaveCount(beforeItemsCount);
   const itemsCount = await TechShopPage.products(page).count();
   console.log(`Count of items after query: ${itemsCount}`);
+}
+
+const isProductPresent = async (page: Page, productName: string): Promise<boolean> => {
+  const productsLocators = await TechShopPage.products(page).all();
+  const found = await Promise.all(productsLocators.map(async pL => (await pL.textContent())!.includes(productName)));
+  return found.some(f => f == true);
+}
+
+async function isProductPresent2(page: Page, productName: string): Promise<boolean> {
+  return (await TechShopPage.products(page).filter({ hasText: new RegExp(`${productName}`) }).count()) > 0;
 }
